27ed1501e09633b0216724c55efb9cfb2546f057
jalmanzal <kwes.zorin@gmail.com>
2020-06-02 20:10:27 -0500

    [ADDED] an easteregg testing endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,6 +9,10 @@ app.get('/mars', function(req, res){
   res.send('Hello Mars!\n');
 })
 
+app.get('/test', function(req, res){
+  res.send('probando?\nProbando?\nProbando, probando, probando...\nPROBANDOOOO!!!!!\nPROBANDOOOO!!!!\nME COPIAS ESPONJA?!!!!')
+})
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
